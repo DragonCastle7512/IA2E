c33ac12c1c8b1a116f0be617fcda9065ec91881c
DragonCastle7512 <kimys75128386@gmail.com>
2026-03-23 20:13:53 +0900

    fix: 환경 변수 로드 위치 조정

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,8 @@
 const express = require('express')
 const path = require('path');
 const app = express()
-const port = process.env.PORT
 require('dotenv').config({ path: path.resolve(__dirname, '..', '.env') });
+const port = process.env.PORT
 
 /* DB 연결 */
 const models = require('./models');
